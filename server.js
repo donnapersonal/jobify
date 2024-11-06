df38cc78eae1b7959d6fb4eb10f3cd90c3e82d1d
donnapersonal <tange0530@gmail.com>
2024-11-06 12:47:23 -0500

    fix front-end

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ if(process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
+
 app.use(cookieParser());
 // Setup express middleware to accept json
 app.use(express.json());
@@ -52,8 +53,8 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use("*", (req, res) => {
